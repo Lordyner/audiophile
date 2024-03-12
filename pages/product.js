5dae5864ac8a74b7ae66a4ed2b7adb7d8ac3d4b2
Thomas André-Lubin <thomas.andrelubin@gmail.com>
2024-03-12 18:11:41 +0100

    Add basic info in pages

---
+++ pages/product.js
@@ -0,0 +1,64 @@
+import { getLogger } from "@/Logging/log-util";
+import Head from "next/head";
+
+export default function Product() {
+
+
+    /* Logger */
+    const logger = getLogger('Product');
+    logger.debug('Product page rendered');
+
+    /* State */
+    const [screenWidth, setScreenWidth] = useState();
+
+    /* Context */
+    const { isMobileResolution, setIsMobileResolution } = useContext(GlobalContext);
+    const { isTabletResolution, setIsTabletResolution } = useContext(GlobalContext);
+    const { setIsDesktopResolution } = useContext(GlobalContext);
+    const { tabletResolution, desktopResolution } = useContext(GlobalContext);
+    const { setIsLoading } = useContext(GlobalContext);
+    const { isMenuOpen } = useContext(GlobalContext);
+
+    /* Router */
+    const router = useRouter();
+
+    /* Functions */
+    const handleMenuDisplay = () => {
+        setScreenWidth(window.screen.width);
+
+        const isMobile = screenWidth < tabletResolution;
+        const isTablet = screenWidth >= tabletResolution && screenWidth < desktopResolution;
+        const isDesktop = screenWidth >= desktopResolution;
+
+        setIsMobileResolution(isMobile);
+        setIsTabletResolution(isTablet);
+        setIsDesktopResolution(isDesktop);
+    }
+
+    useEffect(() => {
+        // Handle menu display
+        handleMenuDisplay();
+        window.addEventListener('resize', handleMenuDisplay);
+
+        // Handle loading spinner
+        router.events.on("routeChangeStart", () => setIsLoading(true));
+        router.events.on("routeChangeComplete", () => setIsLoading(false));
+        console.log()
+    }, [screenWidth])
+
+
+
+
+    return (
+        <>
+            <Head>
+                <title>Product</title>
+                <meta name="description" content="Audiophile" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <div className={`overlay-burger-menu ${isMenuOpen ? 'isActive' : ''}`} />
+
+        </>
+    )
+}
