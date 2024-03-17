b68f2fafe8c81a72c0adff9368e03c69c23dfb35
Thomas André-Lubin <thomas.andrelubin@gmail.com>
2024-03-17 10:26:36 +0100

    Remove sanitizing for now

--- Components/ProductDetail/ProductFeatures.js
+++ Components/ProductDetail/ProductFeatures.js
@@ -3,7 +3,7 @@
 import React, { useEffect, useRef } from 'react';
 import classes from './ProductFeatures.module.css';
 // import DOMPurify from "dompurify";
-import DOMPurify from "isomorphic-dompurify";
+// import DOMPurify from "isomorphic-dompurify";
 const ProductFeatures = ({ features }) => {
     // const sanitizedData = () => ({
     //     __html: DOMPurify.sanitize(features)
@@ -13,7 +13,8 @@ const ProductFeatures = ({ features }) => {
     return (
         <div className={classes.features}>
             <h2>Features</h2>
-            <p className={classes.description} dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(features) }} />
+            {/* <p className={classes.description} dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(features) }} /> */}
+            <p className={classes.description} dangerouslySetInnerHTML={{ __html: features }} />
         </div>
     );
 };
