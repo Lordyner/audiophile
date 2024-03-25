abefe2d26a61043772d27bb4c4aac51e622c67cf
Thomas André-Lubin <thomas.andrelubin@gmail.com>
2024-03-25 18:46:42 +0100

    Plot twist : bug was not resolved

--- Components/TertiaryHighlightedProduct.js
+++ Components/TertiaryHighlightedProduct.js
@@ -19,7 +19,7 @@ const TertiaryHighlightedProduct = () => {
             />
             {/* </div> */}
             <div className={classes.textContainer}>
-                <h2 className={classes.boxProductTitle}>YX1 earphones</h2>
+                <h3 className={classes.boxProductTitle}>YX1 earphones</h3>
                 <Link href='product/yx1-wireless' className={'secondary-link'}>See product</Link>
             </div>
         </div>
